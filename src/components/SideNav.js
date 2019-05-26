8b2b37d04b2eace2a281b2e8b86879b861d2176f
Evan Godon <e.godon@outlook.com>
2019-05-25 20:20:02 -0400

    Create SideNavFooter

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -1,46 +1,99 @@
 import React from 'react';
 import styled from 'styled-components';
-import { color } from 'style';
-import NavLink, { itemHeight } from 'components/NavLink';
+import { Location, Link } from '@reach/router';
+import { ROUTES } from 'constants/index';
+import Icon, { icons } from 'components/Icon';
+import SideNavFooter from 'components/SideNavFooter';
 
-const SideNav = () => {
-  return (
-    <Container>
-      <H1>LOGO</H1>
-      <nav>
-        <NavList>
-          <NavLink to="/">HOME</NavLink>
-          <NavLink to="/about">ABOUT</NavLink>
-          <NavLink to="/experience">EXPERIENCE</NavLink>
-          <NavLink to="/project">PROJECTS</NavLink>
-        </NavList>
-      </nav>
-    </Container>
-  );
+const navIcon = {
+  HOME: icons.home,
+  ABOUT: icons.user,
+  EXPERIENCE: icons.paper,
+  PROJECTS: icons.code,
 };
 
-export const side_padding = '3.2rem';
+const SideNav = () => (
+  <Container>
+    <H1>LOGO</H1>
+    <Location>
+      {({ location }) => (
+        <>
+          <nav>
+            <NavList currentPath={location.pathname}>
+              {Object.keys(ROUTES).map((routeName) => {
+                const isCurrent = location.pathname === ROUTES[routeName];
+                return (
+                  <NavItem key={routeName} isCurrent={isCurrent}>
+                    <Icon src={navIcon[routeName]} size="2rem" fillColor="currentColor" />
+                    <Link to={ROUTES[routeName]}>{routeName}</Link>
+                  </NavItem>
+                );
+              })}
+            </NavList>
+          </nav>
+          {location.pathname !== '/' && <SideNavFooter />}
+        </>
+      )}
+    </Location>
+  </Container>
+);
 
 const Container = styled.div`
-  background-color: ${color.black};
+  background-color: var(--black);
   height: 100vh;
   padding: 3.2rem 0;
-  color: ${color.white};
+  color: var(--white);
+  display: flex;
+  flex-direction: column;
+
+  --side-padding: 3.2rem;
+  --nav-item-height: 3.8rem;
 `;
 
 const H1 = styled.h1`
-  padding: ${side_padding};
+  padding: var(--side-padding);
   margin-bottom: 5rem;
 `;
+
+const lightPosition = {
+  '/': '0',
+  '/about': '100%',
+  '/experience': '200%',
+  '/projects': '300%',
+};
 
 const NavList = styled.ul`
   &:before {
     content: '';
     position: absolute;
     width: 0.5rem;
-    height: ${itemHeight};
+    height: var(--nav-item-height);
     background-color: hotpink;
     box-shadow: 1px 0px 15px hotpink;
+    transition: transform 0.2s ease;
+    transform: translateY(${(p) => lightPosition[p.currentPath]});
+  }
+`;
+
+const NavItem = styled.li`
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  padding-left: 3.4rem;
+  color: ${(p) => (p.isCurrent ? p.theme['--white'] : p.theme['grey-300'])};
+
+  &:hover {
+    color: ${(p) => (p.isCurrent ? p.theme['--white'] : p.theme['grey-300'])};
+  }
+
+  a {
+    flex: 1;
+    height: var(--nav-item-height);
+    line-height: var(--nav-item-height);
+    padding-left: 2.2rem;
+    letter-spacing: 1px;
+    font-size: var(--fs-small);
+    display: block;
   }
 `;
 
---
+++ src/components/SideNavFooter.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import styled from 'styled-components';
+import Icon, { icons } from 'components/Icon';
+import { fadeIn } from 'style';
+import { color } from '../style';
+
+const SideNavFooter = () => (
+  <Footer>
+    <SocialIcons>
+      <Icon src={icons.envelope} />
+      <Icon src={icons.github} viewBox="0 0 10 10" />
+      <Icon src={icons.linkedin} viewBox="0 0 10 10" />
+    </SocialIcons>
+  </Footer>
+);
+
+const Footer = styled.footer`
+  margin-top: auto;
+  animation: ${fadeIn} 0.2s ease;
+`;
+
+const SocialIcons = styled.div`
+  padding: 0 var(--side-padding);
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+
+  svg path {
+    cursor: pointer;
+    fill: var(--white);
+  }
+
+  svg:hover path {
+    fill: var(--grey-300);
+  }
+`;
+
+export default SideNavFooter;
