ae53b532c7fb51440d20d5aabf7eb682dea29820
Evan Godon <e.godon@outlook.com>
2019-06-05 22:03:42 -0400

    Use glitchText on home page headers

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import styled from 'styled-components';
 import SideNavFooter from 'components/SideNavFooter';
+import GlitchText from 'components/GlitchText';
 
 export default () => (
   <Container>
     <HeaderContainer>
-      <H1>Evan Godon</H1>
-      <H2>Front-End Web Developer</H2>
+      <H1>
+        <GlitchText text="Evan Godon" eachLetter/>
+      </H1>
+      <H2><GlitchText text="Front-End Web Developer" eachLetter/></H2>
       <SideNavFooter />
     </HeaderContainer>
   </Container>
@@ -29,13 +32,15 @@ const HeaderContainer = styled.div`
 
 const H1 = styled.h1`
   text-transform: uppercase;
-  letter-spacing: 3px;
+  letter-spacing: 8px;
+  font-size: var(--fs-xlarge);
   font-weight: 300;
   margin-bottom: 1.8rem;
+  position: relative;
 `;
 
 const H2 = styled.h2`
   margin-bottom: 4.8rem;
   font-weight: 300;
-  letter-spacing: 1px;
+  letter-spacing: 3px;
 `;
