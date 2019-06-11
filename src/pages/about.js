b578bc81082295cd1581a30d111122372c68632f
Evan Godon <e.godon@outlook.com>
2019-06-10 21:14:10 -0400

    Add content to about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,44 @@
-import React from 'react'
+import React from 'react';
 import styled from 'styled-components';
+import Profile from 'images/profile.svg';
 
 export default () => (
   <Container>
-    About
+    <ProfileContainer>
+      <Profile />
+    </ProfileContainer>
+    <AboutText>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod maiores similique
+        animi excepturi! Natus hic expedita necessitatibus unde, nostrum ad sequi
+        cupiditate corrupti, repellat accusamus perferendis sapiente animi aut architecto
+        magnam nulla molestiae officia sunt debitis suscipit porro?
+      </p>
+      <br />
+      <p>
+        Nihil perferendis officia maiores dignissimos ex. Quia dolorum quis iure. Earum,
+        eius.
+      </p>
+    </AboutText>
   </Container>
-)
+);
 
 const Container = styled.div`
-  color: green;
-`
+  display: flex;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ProfileContainer = styled.div`
+  width: 14rem;
+  height: 14rem;
+  border-radius: 50%;
+`;
+
+const AboutText = styled.div`
+  max-width: 40rem;
+  font-size: var(--fs-medium);
+  margin: 0 4rem;
+  line-height: 1.2;
+`;
