942b298dad7ac28537d20dd97f4f75453bd07e3c
Evan Godon <e.godon@outlook.com>
2019-06-13 14:52:28 -0400

    Add projects and experience page components

---
+++ src/pages/experience.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export default () => {
+  return <Container>Experience</Container>;
+};
+
+const Container = styled.div`
+  display: flex;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+`;
+
---
+++ src/pages/projects.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export default () => {
+  return <Container>Projects</Container>;
+};
+
+const Container = styled.div`
+  display: flex;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+`;
