014b69e0d9383eb5e134674edf0b60c884c59ed6
Evan Godon <e.godon@outlook.com>
2019-06-18 17:22:22 -0400

    Create general layout for experience page

---
+++ src/components/WorkExperience.js
@@ -0,0 +1,102 @@
+import React from 'react';
+import styled from 'styled-components';
+import RightChevronIcon from 'static/icons/chevron-right.svg';
+
+const WorkExperience = ({ workExperience }) => {
+  const { company, timePeriod, position, techStack, responsabilities } = workExperience;
+
+  return (
+    <Container>
+      <TimePeriod>{timePeriod}</TimePeriod>
+      <TimeLine>
+        <IconContainer>
+          {/* <MapPinIcon width="1.6rem" /> */}
+          {company[0]}
+        </IconContainer>
+      </TimeLine>
+      <Details>
+        <Company>{company}</Company>
+        <Position>{position}</Position>
+        <TechStack>Tech Stack: {techStack.join(', ')}</TechStack>
+        <ResponsibilityList>
+          {responsabilities.map((responsibility) => (
+            <Responsibility>
+              <RightChevronIcon color="var(--color-primary)" width="1.8rem" />
+              {responsibility}
+            </Responsibility>
+          ))}
+        </ResponsibilityList>
+      </Details>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  display: grid;
+  grid-template-columns: 16rem 8rem 1fr;
+  margin-bottom: 6rem;
+`;
+
+const TimePeriod = styled.span`
+  font-size: var(--fs-small);
+  color: var(--grey-300);
+`;
+
+const TimeLine = styled.div`
+  display: flex;
+  justify-content: center;
+  position: relative;
+
+  &::before {
+    content: '';
+    position: absolute;
+    display: block;
+    height: 130%;
+    width: 0.2rem;
+    background-color: var(--color-secondary);
+  }
+`;
+
+const IconContainer = styled.div`
+  font-family: 'anuratiregular', 'Helvitica';
+  position: relative;
+  top: calc(var(--size) / 3 * -1);
+  background-color: var(--color-secondary);
+  border-radius: 50%;
+  --size: 3.6rem;
+  height: var(--size);
+  width: var(--size);
+  line-height: var(--size);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Company = styled.h3`
+  margin-bottom: 0.6rem;
+`;
+
+const Position = styled.h4`
+  margin-bottom: 1.2rem;
+  color: var(--grey-200);
+`;
+
+const Details = styled.div``;
+
+const TechStack = styled.div`
+  margin-bottom: 3.2rem;
+  font-size: var(--fs-medium);
+  color: var(--grey-200);
+`;
+
+const ResponsibilityList = styled.ul`
+  font-size: var(--fs-medium);
+`;
+
+const Responsibility = styled.li`
+  margin-bottom: 0.4rem;
+  display: flex;
+  align-items: center;
+`;
+
+export default WorkExperience;
--- src/pages/experience.js
+++ src/pages/experience.js
@@ -1,18 +1,86 @@
 import React from 'react';
 import styled from 'styled-components';
 import H1 from 'components/H1';
+import WorkExperience from 'components/WorkExperience';
 
 export default () => {
   return (
     <Container>
-      <H1>Experience</H1>
+      <Content>
+        <H1 largeMargin>Experience</H1>
+
+        <ExperienceList>
+          {workExperiences.map((workExperience) => (
+            <WorkExperience workExperience={workExperience} />
+          ))}
+        </ExperienceList>
+      </Content>
     </Container>
   );
 };
 
+const workExperiences = [
+  {
+    company: 'Bmad',
+    position: 'Front-End Web Developer',
+    timePeriod: 'January 2018 - August 2018',
+    techStack: [
+      'React',
+      'Redux',
+      'Redux-Saga',
+      'React 360',
+      'Styled-Components',
+      'Jest',
+      'Django',
+      'Flask',
+      'MySQL',
+    ],
+    responsabilities: [
+      'Collaborated with team members using the Scrum framework and sprint planning.',
+      'Responsible for the front-end infrastructure of a web and electron application using React.',
+      'Developed the UI while implementing the design specifications provided by the UX/UI designer.',
+      'Created many reusable React components and helped grow the UI library of the application.',
+      'Developed the registration and login pages and built the UI for a virtual reality platform using React 360.',
+    ],
+  },
+  {
+    company: 'Bmad',
+    position: 'Front-End Web Developer',
+    timePeriod: 'January 2018 - August 2018',
+    techStack: [
+      'React',
+      'Redux',
+      'Redux-Saga',
+      'React 360',
+      'Styled-Components',
+      'Jest',
+      'Django',
+      'Flask',
+      'MySQL',
+    ],
+    responsabilities: [
+      'Collaborated with team members using the Scrum framework and sprint planning.',
+      'Responsible for the front-end infrastructure of a web and electron application using React.',
+      'Developed the UI while implementing the design specifications provided by the UX/UI designer.',
+      'Created many reusable React components and helped grow the UI library of the application.',
+      'Developed the registration and login pages and built the UI for a virtual reality platform using React 360.',
+    ],
+  },
+];
+
 const Container = styled.div`
   display: flex;
+  flex-direction: column;
   height: 100%;
-  justify-content: center;
+  align-items: center;
+  overflow-y: auto;
+`;
+
+const Content = styled.div`
+  margin-top: 16%;
+  display: flex;
+  flex-direction: column;
   align-items: center;
 `;
+
+const ExperienceList = styled.ul``;
