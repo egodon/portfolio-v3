2b5cf227e0306f36e64f7db0b84da51d98cb2cf3
Evan Godon <e.godon@outlook.com>
2019-08-06 13:10:05 -0400

    Style changes to button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,23 +1,28 @@
 import styled from 'styled-components';
 
 const Button = styled.button`
+  text-transform: uppercase;
   border: 1px solid currentColor;
   background-color: transparent;
   color: ${(props) => props.color || props.theme.__color_secondary};
-  padding: 1.2rem 2.4rem;
+  padding: 0 2.4rem;
   cursor: pointer;
   transition: all 0.1s;
-  font-size: var(--fs-small);
-  opacity: 0.9;
+  font-size: var(--fs-xsmall);
+  --height: 4.2rem;
+  height: var(--height);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  opacity: 1;
 
   &:hover {
-    opacity: 1;
+    opacity: 0.9;
   }
 
   &:active {
-    background-color: var(--color-secondary);
+    /* background-color: var(--color-secondary); */
   }
 `;
-
 
 export default Button;
