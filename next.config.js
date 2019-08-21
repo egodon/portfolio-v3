851a173825dfb79499836b1e8e3749e14c3e34c9
Evan Godon <e.godon@outlook.com>
2019-08-20 21:18:56 -0400

    Merging master

--- next.config.js
+++ next.config.js
@@ -1,37 +1,52 @@
 const path = require('path');
-const withImages = require('next-images');
+const withPlugins = require('next-compose-plugins');
+const images = require('next-images');
+const optimizedImages = require('next-optimized-images');
 
-module.exports = withImages({
-  onDemandEntries: {
-    pagesBufferLength: 4,
-  },
-  exclude: [
-    path.resolve(__dirname, 'static/icons'),
-  ],
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ['@svgr/webpack'],
-    });
+module.exports = withPlugins([
+  [images, optimizedImages],
+  {
+    exclude: [path.resolve(__dirname, 'static/icons')],
+    webpack(config) {
+      config.module.rules.push({
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+      });
 
-    config.module.rules.push({
-      test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
-      use: {
-        loader: 'url-loader',
-        options: {
-          // Limit at 50k. Above that it emits separate files
-          limit: 100000,
+      config.module.rules.push({
+        test: /(png|jpe?g)/,
+        loaders: [
+          {
+            loader: 'lqip-loader',
+            options: {
+              path: '/media', // your image going to be in media folder in the output dir
+              name: '[name].[ext]', // you can use [hash].[ext] too if you wish,
+              base64: true, // default: true, gives the base64 encoded image
+              palette: true, // default: false, gives the dominant colours palette
+            },
+          },
+        ],
+      });
 
-          // url-loader sets mimetype if it's passed.
-          // Without this it derives it from the file extension
-          mimetype: 'application/font-woff',
+      config.module.rules.push({
+        test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            // Limit at 50k. Above that it emits separate files
+            limit: 100000,
 
-          // Output below fonts directory
-          name: './fonts/[name].[ext]',
+            // url-loader sets mimetype if it's passed.
+            // Without this it derives it from the file extension
+            mimetype: 'application/font-woff',
+
+            // Output below fonts directory
+            name: './fonts/[name].[ext]',
+          },
         },
-      },
-    });
+      });
 
-    return config;
+      return config;
+    },
   },
-});
+]);
