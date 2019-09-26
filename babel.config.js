aac61c12f997caa4845c8a133604c1dddb346201
Evan Godon <e.godon@outlook.com>
2019-09-25 22:29:42 -0400

    Remove sc display names in production

---
+++ babel.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  presets: ['next/babel'],
+  plugins: [
+    [
+      'styled-components',
+      { ssr: true, displayName: process.env.NODE_ENV === 'developement' },
+    ],
+    [
+      'module-resolver',
+      {
+        root: ['./'],
+        alias: {
+          components: './src/components',
+          css: './src/css',
+          constants: './src/constants',
+          style: './src/style',
+          utils: './src/utils',
+          hooks: './src/hooks',
+        },
+      },
+    ],
+  ],
+};
