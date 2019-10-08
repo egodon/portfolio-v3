1a5019ff3cdef2d84cd04c94db459ca07dcd022e
Evan Godon <e.godon@outlook.com>
2019-10-08 17:04:31 -0400

    Add logrocket

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,8 +5,13 @@ import { RouterContextProvider } from 'hooks/useRouter';
 import { PageTransition } from 'components/PageTransition';
 import Favicon from 'components/head/Favicon';
 import MetaTags from 'components/head/MetaTags';
+import LogRocket from 'logrocket';
 
 class MyApp extends App {
+  componentDidMount() {
+    LogRocket.init('tj21zp/my-portfolio');
+  }
+
   render() {
     const { Component: SsrComponent, pageProps: ssrPageProps } = this.props;
 
