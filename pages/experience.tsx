7deb7d7d5ff70ed82badacd13785baf2691686c8
Evan Godon <e.godon@outlook.com>
2019-10-11 18:12:04 -0400

    fix cv link

--- pages/experience.tsx
+++ pages/experience.tsx
@@ -20,7 +20,7 @@ const Experience: NextPage = () => {
           <H1 largeMargin>{header}</H1>
           <CvButton
             as="a"
-            href="www.bit.ly/Evan-Godon-Resume"
+            href="https://bit.ly/Evan-Godon-Resume"
             target="_blank"
             rel="noopener"
           >
